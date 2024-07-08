fa37e6a3c064c52ce6fe0fcc7f75a5e351802dc0
ataras2 <ataras2@gmail.com>
2024-07-08 15:31:56 +1000

    dichroic switch working

--- src/ZaberMotor.py
+++ src/ZaberMotor.py
@@ -10,27 +10,77 @@ Ideas:
 import zaber_motion
 
 from zaber_motion.ascii import Connection
-
-connection = Connection.open_serial_port("COM3")
-connection.enable_alerts()
-
-device_list = connection.detect_devices()
-print("Found {} devices".format(len(device_list)))
+import time
 
 
-for dev in device_list:
-    print(
-        f"Device {dev.device_id} with serial number {dev.serial_number} with {dev.axis_count} axes"
-    )
+class BifrostDichroic:
+    def __init__(self, device) -> None:
+        self.device = device
+        self.axis = device.get_axis(1)
+        self.positions = {
+            "H": 132.32,
+            "J": 62.32,
+            "out": 0.0,
+        }
 
+        assert self.device.name == "X-LSM150A-SE03"
 
-connection.close()
+        if not self.axis.is_homed:
+            self.axis.home(wait_until_idle=True)
 
+        self.current_position = "out"
+        self.set_position(self.current_position)
 
-class ZaberLinearStage:
-    def __init__(self) -> None:
-        pass
+    def set_position(self, position):
+        """Move the optic to the desired position"""
+
+        if position not in self.positions:
+            raise ValueError(f"Position {position} not in {self.positions.keys()}")
+
+        self.axis.move_absolute(
+            self.positions[position],
+            unit=zaber_motion.Units.LENGTH_MILLIMETRES,
+            wait_until_idle=False,
+        )
+        self.current_position = position
+
+    def get_position(self):
+        """Read the position from the device and check that it is consistent"""
+        pos = self.axis.get_position(unit=zaber_motion.Units.LENGTH_MILLIMETRES)
+        print(f"position: {pos:.3f}mm")
+        for key, value in self.positions.items():
+            if abs(pos - value) < 0.1:
+                return key
+        return "unknown"
 
 
 if __name__ == "__main__":
-    pass
+    connection = Connection.open_serial_port("COM3")
+    connection.enable_alerts()
+
+    device_list = connection.detect_devices()
+    print("Found {} devices".format(len(device_list)))
+
+    dichroics = []
+    for dev in device_list:
+        if dev.name == "X-LSM150A-SE03":
+            dichroics.append(BifrostDichroic(dev))
+    print(f"Found {len(dichroics)} dichroics")
+
+    for dichroic in dichroics:
+        dichroic.set_position("J")
+
+    while dichroics[0].get_position() != "J":
+        pass
+
+    time.sleep(0.5)
+    for dichroic in dichroics:
+        print(dichroic.get_position())
+
+    for i in range(10):
+        time.sleep(0.5)
+
+        pos = dichroics[0].axis.get_position(unit=zaber_motion.Units.LENGTH_MILLIMETRES)
+        print(f"position: {pos:.3f}mm")
+
+    connection.close()
