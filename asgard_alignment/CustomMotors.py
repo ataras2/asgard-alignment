4def87f33a9e2db677b91977be15a3cbeb848178
ataras2 <ataras2@gmail.com>
2025-02-11 17:11:29 +0100

    mirrorflipper written

--- asgard_alignment/CustomMotors.py
+++ asgard_alignment/CustomMotors.py
@@ -45,3 +45,67 @@ class MFF101(asgard_alignment.ESOdevice.Motor):
 
     def standby(self):
         pass
+
+
+class MirrorFlipper(asgard_alignment.ESOdevice.Motor):
+    def __init__(
+        self,
+        name,
+        semaphore_id,
+        controllino_controller,
+        modulation_value,
+        delay_time,
+    ) -> None:
+        named_pos = {"down": 0, "up": 1}
+        super().__init__(
+            name,
+            semaphore_id,
+            named_positions=named_pos,
+        )
+
+        self._controller = controllino_controller
+        self._state = None
+
+        self._modulation_value = modulation_value
+        self._delay_time = delay_time
+
+    def _flip_up(self):
+        self._controller.flip_up(self.name, self._modulation_value, self._delay_time)
+        self._state = "up"
+
+    def _flip_down(self):
+        self._controller.flip_down(self.name, self._modulation_value, self._delay_time)
+        self._state = "down"
+
+    def move_abs(self, position):
+        if isinstance(position, str):
+            position = self._named_positions[position]
+
+        if position == 0:
+            self._flip_down()
+        if position == 1:
+            self._flip_up()
+
+    def move_relative(self, position: float):
+        pass
+
+    def read_state(self):
+        return self._state
+
+    def setup(self, value):
+        pass
+
+    def disable(self):
+        pass
+
+    def enable(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def online(self):
+        pass
+
+    def standby(self):
+        pass
