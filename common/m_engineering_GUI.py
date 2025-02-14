f689d084bbf19641fb137d428ce5b6b8d7c0e2ca
Adam Taras <ataras2@gmail.com>
2025-02-15 03:57:28 +1100

    add c red conf

--- common/m_engineering_GUI.py
+++ common/m_engineering_GUI.py
@@ -1384,8 +1384,15 @@ with col_main:
                 "State",
                 "Connect?",
             ]
-            keys = ["axis", "motor_type", "is_connected", "state", "connect_button", "reset_button"]
-            col_widths = [1, 1, 1, 5, 1,1]
+            keys = [
+                "axis",
+                "motor_type",
+                "is_connected",
+                "state",
+                "connect_button",
+                "reset_button",
+            ]
+            col_widths = [1, 1, 1, 5, 1, 1]
 
             st.write("Health of all motors")
 
@@ -1422,7 +1429,7 @@ with col_main:
                     elif keys[j] == "reset_button":
                         if data[i]["motor_type"] == "LS16P":
                             if col.button("Reset", key=f"reset_{i}"):
-                                message = f"init {data[i]['axis']}"
+                                message = f"reset {data[i]['axis']}"
                                 send_and_get_response(message)
                     else:
                         col.write(data[i][keys[j]])
