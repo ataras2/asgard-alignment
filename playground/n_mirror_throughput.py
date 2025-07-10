a8831c27ec742de7ddb9d785cd3a6b61b38595c1
ataras2 <ataras2@gmail.com>
2025-07-10 17:05:13 -0400

    toy model of mv pup

--- playground/n_mirror_throughput.py
+++ playground/n_mirror_throughput.py
@@ -15,7 +15,7 @@ w = 256
 true_offset = 8
 hole = circle_mask((w, w), (w // 2+true_offset, w // 2), w // 4)
 
-blob_radius = 0.99 * w // 4
+blob_radius = 0.95 * w // 4
 
 offsets = np.linspace(-0.3 * blob_radius, 0.3 * blob_radius, 7)
 flux = np.zeros(len(offsets))
@@ -23,21 +23,29 @@ flux = np.zeros(len(offsets))
 # plt.imshow(hole)
 
 for i, offset in enumerate(offsets):
-    blob = circle_mask((w, w), (w // 2 + offset, w // 2 + 0), blob_radius)
+    blob = circle_mask((w, w), (w // 2 + offset, w // 2 + 1.5), blob_radius)
     # plt.imshow(blob, alpha=0.5)
     flux[i] = np.sum(blob & hole)
 
 flux /= np.max(flux)  # normalise
 
 noise = np.random.normal(0, 0.005, len(flux))
-flux += noise  # add some noise
+# flux += noise  # add some noise
+
+offsets/= 100
+true_offset /= 100
 
 # curve fit - this is of the form
 # y = -m|x-a| -m|x-b| + c
 from scipy.optimize import curve_fit
+import time
 def fit_func(x, m, a, b, c):
     return -m * np.abs(x - a) - m * np.abs(x - b) + c
-params, _ = curve_fit(fit_func, offsets, flux, p0=[1, -1, 1, 0])
+start = time.time()
+n_loops = 1000
+for _ in range(n_loops):
+    params, _ = curve_fit(fit_func, offsets, flux, p0=[1, -0.05, 0.05, 1])
+print(f"Curve fit took {(time.time() - start)/n_loops:.5f} seconds")
 
 # estimate is (b + a) / 2
 estimate_offset = (params[1] + params[2]) / 2
