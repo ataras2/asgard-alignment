8db9aa5c626de06618a9e1c4b50f11908c3c93ff
ataras2 <ataras2@gmail.com>
2025-07-12 14:44:36 -0400

    logner pause

--- asgard_alignment/cmd_scripts/startup_instrument.py
+++ asgard_alignment/cmd_scripts/startup_instrument.py
@@ -93,6 +93,7 @@ def power_on_instrument_only():
     for cmd in cmds:
         print(f"Running command: {cmd}")
         res = os.system(cmd)
+        time.sleep(5)
         if res != 0:
             print(f"Command '{cmd}' failed. Exiting.")
             sys.exit(1)
