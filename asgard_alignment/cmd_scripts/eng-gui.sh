96fd2404a5f8c476358805a5fd3019eee0d6cc0c
Adam Taras <ataras2@gmail.com>
2025-07-12 16:20:38 +0000

    correct port

--- asgard_alignment/cmd_scripts/eng-gui.sh
+++ asgard_alignment/cmd_scripts/eng-gui.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cd /home/asg/Progs/repos/asgard-alignment
 export PYTHONPATH="/home/asg/Progs/repos/asgard-alignment"
-xterm -hold -title "Eng Gui Streamlit (installed)" -e streamlit run asgard_alignment/cmd_scripts/m_engineering_GUI.py &
+xterm -hold -title "Eng Gui Streamlit (installed)" -e streamlit run asgard_alignment/cmd_scripts/engineering_GUI.py --server.port 8501 &
