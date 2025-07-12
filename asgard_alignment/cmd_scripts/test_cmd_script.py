00e09646a63ba7738989fb6db454f85502511f5e
Adam Taras <ataras2@gmail.com>
2025-07-12 14:47:16 +0000

    new scripts working now

---
+++ asgard_alignment/cmd_scripts/test_cmd_script.py
@@ -0,0 +1,4 @@
+import asgard_alignment.Engineering
+
+def main():
+    print(asgard_alignment.Engineering.get_matricies("c_red_one_focus"))
--- playground/test_cmd_script.py
+++
@@ -1,4 +0,0 @@
-import asgard_alignment.Engineering
-
-def main():
-    asgard_alignment.Engineering.get_matricies("c_red_one_focus")
