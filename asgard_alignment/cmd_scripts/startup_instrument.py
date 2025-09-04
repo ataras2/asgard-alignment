5bf5f65e58e26b5a6221524fbff4c1863309a63b
ataras2 <ataras2@gmail.com>
2025-09-04 08:40:43 -0400

    pause during startup

--- asgard_alignment/cmd_scripts/startup_instrument.py
+++ asgard_alignment/cmd_scripts/startup_instrument.py
@@ -59,6 +59,9 @@ def power_on_all():
         if res != 0:
             print(f"Command '{cmd}' failed. Exiting.")
             sys.exit(1)
+        # if not the last, wait a bit
+        if cmd != cmds[-1]:
+            time.sleep(2)
 
     print("All commands executed successfully. Instrument startup complete.")
     print("Load a state using the gui, and run 'fetch' on the camera server")
