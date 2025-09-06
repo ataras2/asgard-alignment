a6e3f2007b597cec7e495ae2093794516ef84524
ataras2 <ataras2@gmail.com>
2025-09-06 11:45:17 -0400

    analysis of second run

--- playground/analyse_pupil_move_data.py
+++ playground/analyse_pupil_move_data.py
@@ -5,20 +5,21 @@ import matplotlib.pyplot as plt
 
 # %%
 
-pth = "../data/pupil_moves"
+pth = "../data/pupil_moves/a1"
 fname = f"heimdallr_pupil_beam{1}.npz"
 
 data = np.load(f"{pth}/{fname}")
 list(data.keys())
 # %%
-positons = [data[f"meas_locs_{i}"] for i in ["x", "y"]]
-fluxes = [data[f"fluxes_{i}"] for i in ["x", "y"]]
-optimal_offsets = [data[f"optimal_offset_{i}"] for i in ["x", "y"]]
+positions = [data[f"meas_locs_{i}"] for i in ["x", "y","x"]]
+fluxes = [data[f"fluxes_{i}"] for i in ["x", "y","x2"]]
+optimal_offsets = [data[f"optimal_offset_{i}"] for i in ["x", "y","x2"]]
 # %%
 plt.figure()
-plt.plot(positons[0], fluxes[0], "o-", label="x", color="C0")
-plt.plot(positons[1], fluxes[1], "o-", label="y", color="C1")
-for i in range(2):
+plt.plot(positions[0], fluxes[0], "o-", label="x", color="C0")
+plt.plot(positions[1], fluxes[1], "o-", label="y", color="C1")
+plt.plot(positions[2], fluxes[2], "o-", label="x2", color="C2")
+for i in range(3):
     plt.axvline(optimal_offsets[i], color=f"C{i}", ls="--")
 plt.xlabel("Position")
 plt.ylabel("Flux")
@@ -33,13 +34,14 @@ fig, axs = plt.subplots(2, 2, figsize=(10, 8), sharex=True, sharey=True)
 
 for beam in range(1, n_beams + 1):
     data = np.load(f"{pth}/heimdallr_pupil_beam{beam}.npz")
-    positons = [data[f"meas_locs_{i}"] for i in ["x", "y"]]
-    fluxes = [data[f"fluxes_{i}"] for i in ["x", "y"]]
-    optimal_offsets = [data[f"optimal_offset_{i}"] for i in ["x", "y"]]
+    positions = [data[f"meas_locs_{i}"] for i in ["x", "y","x"]]
+    fluxes = [data[f"fluxes_{i}"] for i in ["x", "y","x2"]]
+    optimal_offsets = [data[f"optimal_offset_{i}"] for i in ["x", "y","x2"]]
     ax = axs.flat[beam - 1]
-    ax.plot(positons[0], fluxes[0], "o-", label="x", color="C0")
-    ax.plot(positons[1], fluxes[1], "o-", label="y", color="C1")
-    for i in range(2):
+    ax.plot(positions[0], fluxes[0], "o-", label="x", color="C0")
+    ax.plot(positions[1], fluxes[1], "o-", label="y", color="C1")
+    ax.plot(positions[2], fluxes[2], "o-", label="x2", color="C2")
+    for i in range(3):
         ax.axvline(optimal_offsets[i], color=f"C{i}", ls="--")
     ax.set_title(f"Beam {beam}")
     if beam in [3, 4]:
